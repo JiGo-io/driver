7b2de568f5bb7e6d7089d26049c7442c759a0c05
JiGo-io <Volume123!>
2020-03-09 20:30:00 +0200

    drawerLayout

---
+++ app/src/main/java/com/telran/draverlayaut/MainActivity.java
@@ -0,0 +1,53 @@
+package com.telran.draverlayaut;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import static com.telran.draverlayaut.R.string.*;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dl = findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl, open, close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.tit1) {
+                    Toast.makeText(MainActivity.this, "title 1", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.tit2) {
+                    Toast.makeText(MainActivity.this, "title 2", Toast.LENGTH_SHORT).show();
+                } else if (id == R.id.tit3) {
+                    Toast.makeText(MainActivity.this, "title 3", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+}
